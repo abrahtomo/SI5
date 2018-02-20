4b14c369bd1f0743ba60ae72161ce969189e9177
Tamas Abraham <tomoabraham92@gmail.com>
2018-02-20 10:52:34 +0100

    Handling unkown api results

--- server.py
+++ server.py
@@ -8,36 +8,78 @@ app = Flask(__name__)
 @app.route('/first-page')
 def show_first_page():
     planets = datahandler.get_first_page()
+    for planet in planets:
+        if planet['surface_water'] != 'unknown':
+            planet['surface_water'] = planet['surface_water'] + '%'
+        if planet['diameter'] != 'unknown':
+            planet['diameter'] = planet['diameter'] + 'km'
+        if planet['population'] != 'unknown':
+            planet['population'] = planet['population'] + ' people'
     return render_template("first_page.html", planets=planets)
 
 
 @app.route('/second-page', methods=['GET', 'POST'])
 def show_second_page():
     planets = datahandler.get_second_page()
+    for planet in planets:
+        if planet['surface_water'] != 'unknown':
+            planet['surface_water'] = planet['surface_water'] + '%'
+        if planet['diameter'] != 'unknown':
+            planet['diameter'] = planet['diameter'] + 'km'
+        if planet['population'] != 'unknown':
+            planet['population'] = planet['population'] + ' people'
     return render_template('second_page.html', planets=planets)
 
 
 @app.route('/third-page', methods=['GET', 'POST'])
 def show_third_page():
     planets = datahandler.get_third_page()
+    for planet in planets:
+        if planet['surface_water'] != 'unknown':
+            planet['surface_water'] = planet['surface_water'] + '%'
+        if planet['diameter'] != 'unknown':
+            planet['diameter'] = planet['diameter'] + 'km'
+        if planet['population'] != 'unknown':
+            planet['population'] = planet['population'] + ' people'
     return render_template('third_page.html', planets=planets)
 
 
 @app.route('/fourth-page', methods=['GET', 'POST'])
 def show_fourth_page():
     planets = datahandler.get_fourth_page()
+    for planet in planets:
+        if planet['surface_water'] != 'unknown':
+            planet['surface_water'] = planet['surface_water'] + '%'
+        if planet['diameter'] != 'unknown':
+            planet['diameter'] = planet['diameter'] + 'km'
+        if planet['population'] != 'unknown':
+            planet['population'] = planet['population'] + ' people'
     return render_template('fourth_page.html', planets=planets)
 
 
 @app.route('/fifth-page', methods=['GET', 'POST'])
 def show_fifth_page():
     planets = datahandler.get_fifth_page()
+    for planet in planets:
+        if planet['surface_water'] != 'unknown':
+            planet['surface_water'] = planet['surface_water'] + '%'
+        if planet['diameter'] != 'unknown':
+            planet['diameter'] = planet['diameter'] + 'km'
+        if planet['population'] != 'unknown':
+            planet['population'] = planet['population'] + ' people'
     return render_template('fifth_page.html', planets=planets)
 
 
 @app.route('/sixth-page', methods=['GET', 'POST'])
 def show_last_page():
     planets = datahandler.get_last_page()
+    for planet in planets:
+        if planet['surface_water'] != 'unknown':
+            planet['surface_water'] = planet['surface_water'] + '%'
+        if planet['diameter'] != 'unknown':
+            planet['diameter'] = planet['diameter'] + 'km'
+        if planet['population'] != 'unknown':
+            planet['population'] = planet['population'] + ' people'
     return render_template('sixth_page.html', planets=planets)
 
 
