1aca2e8371d72a014db3c5aebbe264441359fde6
Tamas Abraham <tomoabraham92@gmail.com>
2018-02-22 12:50:48 +0100

    Bugfix

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 function addButton() {
     var lastCells = document.getElementsByClassName('residents');
     for (let i=0; i<lastCells.length; i++) {
-        var button = "<button class=\"btn btn-outline-secondary\" role=\"button\" data-toggle=\"modal\" data-target=\"#myModal\">"+lastCells[i].innerHTML+"</button>";
+        let button = "<button class=\"btn btn-outline-secondary\" role=\"button\" data-toggle=\"modal\" data-target=\"#myModal\">"+lastCells[i].innerHTML+"</button>";
         if (lastCells[i].innerHTML !== 'No known residents') {
             lastCells[i].innerHTML=button;
         }
