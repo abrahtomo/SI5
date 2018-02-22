bde3786bfe330a283149146b1ffa0c4bf8104c83
Tamas Abraham <tomoabraham92@gmail.com>
2018-02-22 12:49:28 +0100

    There is a table in the modal

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,5 @@
 function addButton() {
     var lastCells = document.getElementsByClassName('residents');
-
     for (let i=0; i<lastCells.length; i++) {
         var button = "<button class=\"btn btn-outline-secondary\" role=\"button\" data-toggle=\"modal\" data-target=\"#myModal\">"+lastCells[i].innerHTML+"</button>";
         if (lastCells[i].innerHTML !== 'No known residents') {
